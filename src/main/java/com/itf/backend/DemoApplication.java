87e989c2a9ef55a4c77e35ab20bd5dcd75be8a36
Rui Zhao <christianoray@gmail.com>
2021-04-14 18:51:05 +0800

    User account finished

--- src/main/java/com/itf/backend/DemoApplication.java
+++ src/main/java/com/itf/backend/DemoApplication.java
@@ -1,11 +1,17 @@
 package com.itf.backend;
 
+import com.itf.backend.utils.FileStorageProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+
 @MapperScan("com.itf.backend.mapper")
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class DemoApplication {
 
     public static void main(String[] args) {
